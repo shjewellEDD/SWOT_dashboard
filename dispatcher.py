94f40d070eeeb208c399312acb17297bacbb63af
shjewellEDD <scott.jewell@noaa.gov>
2022-05-09 09:55:29 -0700

    Eliminated unecessary Flask import

--- dispatcher.py
+++ dispatcher.py
@@ -1,11 +1,9 @@
 
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-import flask
 from test_dashboard import server as test
 import landing_page
 
 
 app = DispatcherMiddleware(landing_page, {
     '/prawler/test': test,
-#    '/main': landing
 })
